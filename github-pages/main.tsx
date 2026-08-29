5932258eee9e9323a49a77a40f46fa799735a657
lfeelterrible-lab <lfeelterrible@qq.com>
2026-08-29 09:00:53 +0800

    Publish HabiTogether pair sync app

--- github-pages/main.tsx
+++ github-pages/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import GeoNote from '../app/page';
-import '../app/globals.css';
+import HabiTogether from '../app/components/HabiTogether';
+import '../app/habi-together.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Missing app root');
 
 createRoot(root).render(
   <StrictMode>
-    <GeoNote />
+    <HabiTogether />
   </StrictMode>,
 );
